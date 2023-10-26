17ca72044928790bea1255405de2f8e401da6e34
FarzinKhosravi <khosravii.farzin@gmail.com>
2023-10-26 14:22:51 +0330

    added character detail component

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,7 +1,46 @@
 function CharacterDetail() {
   return (
     <div>
-
+      <div className="hidden">
+        <img
+          className="block h-14 w-14 rounded-2xl"
+          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          alt="rick-img"
+        />
+      </div>
+      <div className="flex">
+        <div className="flex flex-auto flex-col">
+          <div className="mb-1">
+            <span>👨🏼</span>
+            <span className="ml-1 text-sm font-medium text-slate-300">
+              Rick Sanchez
+            </span>
+          </div>
+          <div>
+            <span className="inline-block h-3 w-3 rounded-full bg-green-600"></span>
+            <span className="ml-2 text-sm font-normal text-slate-300">
+              Alive - Human
+            </span>
+          </div>
+        </div>
+        <div className="ml-4 mt-0.5 flex flex-auto flex-col">
+          <div className="mb-1">
+            <span className="block text-sm text-slate-500">
+              Last known location:
+            </span>
+          </div>
+          <div className="mb-4">
+            <span className="block text-sm font-medium text-slate-300">
+              Earth
+            </span>
+          </div>
+          <div>
+            <button className="inline-flex cursor-pointer items-center justify-center rounded-3xl bg-slate-500 px-4 py-3 text-sm font-medium text-slate-100 transition-all duration-200 hover:bg-slate-700">
+              Add to Favorite
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        0.5: "2px",
+      },
+    },
   },
   // eslint-disable-next-line no-undef
   plugins: [require("@tailwindcss/forms")],
